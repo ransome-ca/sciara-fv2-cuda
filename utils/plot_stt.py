42f8a0fdb0e0954875eeac1421913675923c15fb
Antonino Natale <antonio.natale97@hotmail.com>
2022-02-23 03:41:09 +0100

    Add plots

--- utils/plot_stt.py
+++ utils/plot_stt.py
@@ -3,6 +3,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 fd = open(sys.argv[1], 'r')
 fd = fd.readlines()
@@ -10,20 +11,21 @@ fd = fd[6:]
 fd = [x.strip() for x in fd]
 fd = [x.split() for x in fd]
 fd = [[float(y) for y in x] for x in fd]
+fd[-1][-1] = 0.0
 fd = np.array(fd)
 
-if sys.argv[1] == 'data/2006/output_2006_000000016000_Temperature.stt':
+# if sys.argv[1] == 'data/2006/output_2006_000000016000_Temperature.stt':
 
-    unique, counts = np.unique(fd, return_counts=True)
+#     unique, counts = np.unique(fd, return_counts=True)
 
-    print("Unique values count in Temperature:")
-    for i in range(len(unique)):
-        print(f'Value {unique[i]} has count {counts[i]}')
+#     print("Unique values count in Temperature:")
+#     for i in range(len(unique)):
+#         print(f'Value {unique[i]} has count {counts[i]}')
     
-    plt.imshow(fd, cmap='autumn_r', interpolation='nearest')
+#     plt.imshow(fd, cmap='autumn_r', interpolation='nearest')
 
-else:
-    plt.imshow(fd, cmap='hot', interpolation='nearest')
+# else:
 
+plt.imshow(fd, cmap='hot_r', interpolation='nearest')
 plt.colorbar()
 plt.show()
